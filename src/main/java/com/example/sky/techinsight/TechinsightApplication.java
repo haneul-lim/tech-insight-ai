91b75be557d513b0f00f2609ea4a3db0dbfb1f1b
haneul-lim <limsky369@gmail.com>
2026-05-11 18:45:41 +0900

    Chore: spring boot 프로젝트 생성

---
+++ src/main/java/com/example/sky/techinsight/TechinsightApplication.java
@@ -0,0 +1,13 @@
+package com.example.sky.techinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechinsightApplication.class, args);
+	}
+
+}
